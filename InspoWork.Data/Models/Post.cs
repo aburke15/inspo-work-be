810ee792c5890afbd548303250f668cf4e53a91b
Andre Burke <aburke.dev@gmail.com>
2023-08-03 01:19:46 -0600

    added some basic crud and deleted stuff i did not need

--- InspoWork.Data/Models/Post.cs
+++ InspoWork.Data/Models/Post.cs
@@ -3,6 +3,6 @@ namespace InspoWork.Data.Models;
 public class Post
 {
     public int Id { get; set; }
-    public string Title { get; set; } = null!;
-    public string Body { get; set; } = null!;
+    public string? Title { get; set; }
+    public string? Body { get; set; }
 }
